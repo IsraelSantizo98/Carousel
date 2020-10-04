9392fe1707827407a6c4ec4ddd523f4486af1dae
Israel Santizo <joseisraelss.jiss@gmail.com>
2020-10-03 18:09:29 -0600

    Se termina carousel, se agrego estilos

--- js/main.js
+++ js/main.js
@@ -2,9 +2,9 @@ window.addEventListener('load', function(){
     //Elemento donde queremos agregar el carousel y la opcion como queres tener visualmente el carousel
     new Glider(document.querySelector('.carousel-lista'), {
         //Cuantos slides se muestran
-        slidesToShow: 4,
+        slidesToShow: 1,
         //Al presionar la flecha cuandos slides avanzar
-        slidesToScroll: 4,
+        slidesToScroll: 1,
         //Si se activa que se puede mover con el dedo (celular)
         draggable: true,
         //Indicadores (carousel-indicadores)
@@ -12,6 +12,24 @@ window.addEventListener('load', function(){
         arrows: {
             prev: '.carousel-anterior',
             next: '.carousel-siguiente'
-        }
+        },
+        responsive: [
+            {
+            // screens greater than >= 775px
+            breakpoint: 450,
+            settings: {
+            // Set to `auto` and provide item width to adjust to viewport
+            slidesToShow: 2,
+            slidesToScroll: 2
+            }
+            },{
+            // screens greater than >= 1024px
+                    breakpoint: 800,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3
+                }
+            }
+        ]
     });
 });
